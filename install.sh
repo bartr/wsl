d6ad4253e32c4ca4d863ce6679f7a0a89826fccb
bartr <bartr@microsoft.com>
2024-03-16 18:44:50 -0500

    updated setup

--- install.sh
+++ install.sh
@@ -9,11 +9,12 @@ echo ""
 
 echo ""
 echo ""
-echo "installing configuring $SUDO_USER"
+echo "configuring $SUDO_USER"
 echo ""
 
 # no password for sudo
 echo "$SUDO_USER ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers.d/$SUDO_USER
+echo -e "\n[users]\ndefault=$SUDO_USER\n" >> /etc/wsl.conf
 
 # add to groups
 usermod -aG sudo $SUDO_USER
