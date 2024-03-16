cd5bfae5b9c791acf9a5dafee01f5d6eaf565557
bartr <bartr@microsoft.com>
2024-03-16 18:27:20 -0500

    updated install

--- install.sh
+++ install.sh
@@ -3,82 +3,30 @@
 cd "$(dirname $BASH_SOURCE[0])"
 export dir=$(pwd)
 
-update-alternatives --set iptables /usr/sbin/iptables-legacy
+echo "installing base"
+echo ""
+./install-base.sh
+
+echo ""
+echo ""
+echo "installing configuring $SUDO_USER"
+echo ""
 
 # no password for sudo
 echo "$SUDO_USER ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers.d/$SUDO_USER
 
-# create / add to groups
-groupadd docker
+# add to groups
 usermod -aG sudo $SUDO_USER
 usermod -aG admin $SUDO_USER
 usermod -aG docker $SUDO_USER
 gpasswd -a $SUDO_USER sudo
 
-# add Docker repo
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" > /etc/apt/sources.list.d/docker.list
-
-# add gh cli repo
-curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
-chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" > /etc/apt/sources.list.d/github-cli.list
-
-curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
-curl https://packages.microsoft.com/config/ubuntu/20.04/prod.list > /etc/apt/sources.list.d/msprod.list
-
-apt-get update
-
-apt-get install -y apt-utils dialog apt-transport-https ca-certificates software-properties-common
-apt-get install -y libssl-dev libffi-dev python2-dev build-essential cifs-utils lsb-release gnupg-agent
-apt-get install -y curl git wget nano zsh
-apt-get install -y jq zip unzip httpie dnsutils
-apt-get install -y dotnet-sdk-7.0 golang
-apt-get install -y docker-ce docker-ce-cli containerd.io
-apt-get install -y gh
-ACCEPT_EULA=y apt-get install -y mssql-tools unixodbc-dev
-
-# fix dotnet install issue
-# cp -r /usr/share/dotnet/* /usr/lib/dotnet/
-
-# install kubectl
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
-rm -f kubectl
-
-# install k3d
-wget -q -O - https://raw.githubusercontent.com/rancher/k3d/main/install.sh | TAG=v5.4.6 bash
-
-# install flux
-curl -s https://fluxcd.io/install.sh | bash
-
-# install K9s
-VERSION=$(curl -i https://github.com/derailed/k9s/releases/latest | grep "location: https://github.com/" | rev | cut -f 1 -d / | rev | sed 's/\r//')
-wget https://github.com/derailed/k9s/releases/download/$VERSION/k9s_Linux_amd64.tar.gz
-tar -zxvf k9s_Linux_amd64.tar.gz -C /usr/local/bin
-rm -f k9s_Linux_amd64.tar.gz
-
-# install jp (jmespath)
-VERSION=$(curl -i https://github.com/jmespath/jp/releases/latest | grep "location: https://github.com/" | rev | cut -f 1 -d / | rev | sed 's/\r//')
-wget https://github.com/jmespath/jp/releases/download/$VERSION/jp-linux-amd64 -O /usr/local/bin/jp
-chmod +x /usr/local/bin/jp
-
-# install helm
-curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
-
-# install Kustomize
-cd /usr/local/bin
-curl -s "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh"  | bash
-cd $OLD_PWD
-
+# change shell to zsh
 chsh -s /usr/bin/zsh $SUDO_USER
 touch /home/$SUDO_USER/.zshrc
 
 # change ownership of home directory
 chown -R $SUDO_USER:$SUDO_USER /home/$SUDO_USER
-
-# start the docker service
-service docker start
 
 echo ""
 echo ""
