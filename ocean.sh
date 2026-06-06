32ad5560a6d66ff152bb5370873272e6446cfb4a
bartr <bartr@microsoft.com>
2026-06-06 23:10:20 +0000

    added caddy install

--- ocean.sh
+++ ocean.sh
@@ -19,3 +19,22 @@ git config --global user.email bartr@microsoft.com
 
 cd $dir
 ./scripts/config.sh
+
+echo ""
+echo "installing caddy"
+echo ""
+
+sudo apt-get install -y debian-keyring debian-archive-keyring apt-transport-https
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
+sudo apt-get update
+sudo apt-get install -y caddy
+
+sudo tee /etc/caddy/Caddyfile > /dev/null <<'EOF'
+bartr.dev {
+    reverse_proxy localhost:8080
+}
+EOF
+
+sudo systemctl enable caddy
+sudo systemctl restart caddy
